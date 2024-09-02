9391f6e0d65358249c1ff3a06804b4700df3becb
sungsik.kim <skystar819@gmail.com>
2024-09-03 05:25:42 +0900

    챕터1 추가

---
+++ src/ch01.clj
@@ -0,0 +1,18 @@
+(ns ch01)
+
+(defprotocol Concatenatable
+  (cat [this other]))
+
+(extend-type String
+  Concatenatable
+  (cat [this other]
+    (.concat this other)))
+
+(cat "House" " of Leaves")
+
+(extend-type java.util.List
+  Concatenatable
+  (cat [this other]
+    (concat this other)))
+
+(cat [1 2 3] [4 5 6])
