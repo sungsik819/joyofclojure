c9b10189d6aff68a2475883bd99b367d87ba2e5f
sungsik.kim <skystar819@gmail.com>
2024-09-23 10:28:00 +0900

    매크로 사용하여 구문 변경

--- src/macro.clj
+++ src/macro.clj
@@ -151,3 +151,108 @@
 
 (def x 0)
 (def-watched x (* 12 12))
+
+;; 매크로 사용하여 구문 변경
+;; 매크로를 설계하는 한가지 방법은 원하는 동작에 대한 예제 코드를 작성
+;; 동작시킬 구체적인 애플리케이션 도메인과 사양에 가깝게 작성 되어야 한다.
+;; 그리고 나서 세부 동작을 채우기 위한 매크로와 함수를 작성하면 된다.
+
+;; 소프트웨어 시스템을 설계 할 때 주어진 애플리케이션 도메인을 구성하는 요소들을
+;; 정의해두는 것이 종종 도움이 된다.
+;; 여기에는 요소들 간의 논리적 그룹도 포함 된다.
+;; 이 단계에서의 설게는 그 추상화 단계를 높게 설정 해두고, 구현의 상세 내용을 포함하지 않는다.
+
+;; 인간과 괴물 대결에 관한 도메인 예시
+;; 1. 인간(man) vs 괴물(monster)
+;; 1.1 인간(people)
+;; 1.1.1 사람(Men / Human)
+;; 1.1.1.1 이름(name)
+;; 1.1.1.2 수염(beard)이 있는가?
+
+;; 1.2 괴물(Monster)
+;; 1.2.1 추파카브라(Chupacabra)
+;; 1.2.1.1 염소(goat)를 잡아먹는가?
+
+;; 컴파일 오류 피하기 
+
+;; 일단은 함수가 있다고 가정하고 최상단 부터 작성 한다.
+(comment
+  (domain man-vs-monster
+          (grouping people
+                    (Human "A stock human")
+                    (Man (isa Human)
+                         "A man, baby"
+                         [name]
+                         [has-beard?]))
+          (grouping monsters
+                    (Chupacabra
+                     "A fierce, yet elusive creature"
+                     [eats-goats?]))))
+
+;; 최상단 추상부분 작성후 아래를 작성 한다.
+(defmacro domain [name & body]
+  `{:tag :domain
+    :attrs {:name (str '~name)}
+    :content [~@body]})
+
+;; 컴파일 오류 피하기
+(declare handle-things)
+
+(defmacro grouping [name & body]
+  `{:tag :grouping
+    :attrs {:name (str '~name)}
+    :content [~@(handle-things body)]})
+
+;; 컴파일 오류 피하기
+(declare grok-attrs grok-props)
+
+(defn handle-things [things]
+  (for [t things]
+    {:tag :thing
+     :attrs (grok-attrs (take-while (comp not vector?) t))
+     :content (if-let [c (grok-props (drop-while (comp not vector?) t))]
+                [c]
+                [])}))
+
+;; 백터가 아닌 모든 것 처리
+(defn grok-attrs [attrs]
+  (into {:name (str (first attrs))}
+        (for [a (rest attrs)]
+          (cond
+            (list? a) [:isa (str (second a))]
+            (string? a) [:comment a]))))
+
+;; 속성들을 처리
+(defn grok-props [props]
+  (when props
+    {:tag :properties
+     :attrs nil
+     :content (apply vector (for [p props]
+                              {:tag :property
+                               :attrs {:name (str (first p))}
+                               :content nil}))}))
+
+;; vscode에서 빨간색으로 표시되지만 실행되는 코드임
+(def d (domain man-vs-monster
+               (grouping people
+                         (Human "A stock human")
+                         (Man (isa Human)
+                              "A man, baby"
+                              [name]
+                              [has-beard?]))
+               (grouping monsters
+                         (Chupacabra
+                          "A fierce, yet elusive creature"
+                          [eats-goats?]))))
+
+(:tag d)
+
+(:tag (first (:content d)))
+
+;; xml로 변환 해보기
+(use '[clojure.xml :as xml])
+(xml/emit d)
+
+;; 도메인에 대한 전체적인 구조는 하나의 매크로를 사용하여 기술
+;; 세부 역할들은 구분한다.
+;; 매크로는 항상 데이터를 받아서 데이터를 리턴 한다.
