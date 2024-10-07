0e6bc38cbbf4df3726fa94eee050c64d27899922
sungsik.kim <skystar819@gmail.com>
2024-10-07 09:49:20 +0900

    mock으로 시스템 구성하는 코드 추가

--- src/ch17/joy/patterns/app.clj
+++ src/ch17/joy/patterns/app.clj
@@ -3,3 +3,13 @@
 
 ;; 환경설정으로 시스템 구성하기
 (def config {:type :mock :lib 'joy.patterns.mock})
+
+;; 의존성 주입
+(defn initialize [name cfg]
+  (let [lib (:lib cfg)]
+    (require lib)
+    (di/build-system name cfg)))
+
+(di/handle (initialize :mock-sim config) {})
+
+(initialize :mock-sim config)
